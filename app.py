b988fef48a41dd7ae516cadc9b6ec319c3af457f
monten1234 <touch26.takehiro@icloud.com>
2023-08-24 15:24:23 +0900

    fuck

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
+import streamlit as st
+from azure.cosmos import CosmosClient, PartitionKey
 
+# Azure Cosmos DBの設定
+URL = "YOUR_COSMOSDB_URL"
+KEY = "YOUR_COSMOSDB_KEY"
+DATABASE_NAME = "YOUR_DATABASE_NAME"
+CONTAINER_NAME = "YOUR_CONTAINER_NAME"
+
+client = CosmosClient(URL, credential=KEY)
+database = client.get_database_client(DATABASE_NAME)
+container = database.get_container_client(CONTAINER_NAME)
+
+st.title("Azure Cosmos DB with Streamlit")
+
+# データのアップロード
+st.subheader("Upload Data to Cosmos DB")
+data_key = st.text_input("Enter a key:")
+data_value = st.text_input("Enter a value:")
+
+if st.button("Upload"):
+    item_body = {
+        "id": data_key,
+        "value": data_value
+    }
+    container.upsert_item(item_body)
+    st.success(f"Uploaded data with key: {data_key}")
+
+# データの取得と表示
+st.subheader("Retrieve Data from Cosmos DB")
+query_key = st.text_input("Enter key to retrieve:")
+
+if st.button("Retrieve"):
+    query = f"SELECT * FROM c WHERE c.id = '{query_key}'"
+    results = list(container.query_items(query=query, enable_cross_partition_query=True))
+    
+    if results:
+        st.write(results[0])
+    else:
+        st.warning(f"No data found for key: {query_key}")
